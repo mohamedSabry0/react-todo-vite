a358593b06c8fb3275cbc5e65fcded21be651e17
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-02-20 14:00:51 +0200

    base setup, some components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TodosLogic from './components/TodosLogic'
+
+function App() {
+
+  return (
+    <div className="App">
+       <TodosLogic />
+    </div>
+  )
+}
+
+export default App
